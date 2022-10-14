95e075eb9095bf63b4530def436d48083936f43a
lucascorreaog <lucascorreaog@gmail.com>
2022-10-14 13:41:23 -0300

    Feat: created model

--- archives/models.py
+++ archives/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class File(models.Model):
+    transaction_type = models.CharField(max_length=255)
+    date = models.CharField(max_length=255)
+    amount = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=255)
+    card_number = models.CharField(max_length=255)
+    time = models.CharField(max_length=255)
+    store_owner = models.CharField(max_length=255)
+    store = models.CharField(max_length=255)
+
+class FileUploader(models.Model):
+    upload = models.FileField(upload_to="upFiles/")
+    
